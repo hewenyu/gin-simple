1c532ef7720271f83834db0caada591495264657
yueban <yuebanlaosiji@outlook.com>
2021-03-25 14:24:26 +0800

    update

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
