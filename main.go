d3c0d49a9eceaa73f7d954df6905775b0b2fe43c
yueban <yuebanlaosiji@outlook.com>
2021-04-23 14:06:44 +0800

    loginfo

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"syscall"
 	"time"
 
 	"github.com/fvbock/endless"
 	"github.com/hewenyu/gin-simple/initialize"
+	"github.com/hewenyu/gin-simple/logger"
 )
 
 func main() {
@@ -22,12 +22,11 @@ func main() {
 	server := endless.NewServer(endPoint, Router)
 
 	server.BeforeBegin = func(add string) {
-		log.Printf("Actual pid is %d", syscall.Getpid())
+		logger.Info(fmt.Sprintf("Actual pid is %d", syscall.Getpid()))
 	}
 
 	err := server.ListenAndServe()
 	if err != nil {
-		log.Printf("Server err: %v", err)
+		logger.Error(fmt.Sprintf("Server err: %v", err))
 	}
-	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
